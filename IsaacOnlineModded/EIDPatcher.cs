3e9ef7122ce246e4a80f4131188034391d577933
xADDBx <62178123+xADDBx@users.noreply.github.com>
2024-12-20 04:24:27 +0100

    Fix EID bug; Include EID Patcher in executable

---
+++ IsaacOnlineModded/EIDPatcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsaacModInstaller {
+    public static class EIDPatcher {
+        public static bool Patch(string EIDPath) {
+            var mainLuaPath = Path.Combine(EIDPath, "main.lua");
+            string[] lines = File.ReadAllLines(mainLuaPath);
+            bool mainLuaModified = lines.Any(line => line.Contains("EID.isMultiplayer = true")) && lines.Any(line => line.Contains("EID.isOnlineMultiplayer = true"));
+            if (!mainLuaModified) {
+                for (int i = 0; i < lines.Length; i++) {
+                    if (lines[i].Contains("EID.isMultiplayer = false")) {
+                        lines[i] = lines[i].Replace("EID.isMultiplayer = false", "EID.isMultiplayer = true");
+                    }
+
+                    if (lines[i].Contains("EID.isOnlineMultiplayer = false")) {
+                        lines[i] = lines[i].Replace("EID.isOnlineMultiplayer = false", "EID.isOnlineMultiplayer = true");
+                    }
+                }
+                File.WriteAllLines(mainLuaPath, lines);
+            }
+            var eidAPIPath = Path.Combine(EIDPath, "features", "eid_api.lua");
+            lines = File.ReadAllLines(eidAPIPath);
+            bool isFirstContained = lines.Any(line => line.Contains("if EID.isOnlineMultiplayer and Game():GetLevel():GetStage() >= LevelStage.Home then"));
+            bool eidApiModified = false;
+            var lineList = lines.ToList();
+            if (isFirstContained) {
+                int firstLine = lineList.IndexOf(lines.First(line => line.Contains("if EID.isOnlineMultiplayer and Game():GetLevel():GetStage() >= LevelStage.Home then")));
+                eidApiModified = lineList[firstLine + 1].Contains("return listUpdatedForPlayers -- Calling player:HasCollectible can cause a crash after beating The Beast in R+ Coop") && lineList[firstLine + 2].Contains("end");
+            }
+            if (!eidApiModified) {
+                int threeBeforePatch = lineList.IndexOf(lines.First(line => line.Contains("return listUpdatedForPlayers -- dont evaluate when bad data is present")));
+                lineList.Insert(threeBeforePatch + 2, "\t\t");
+                lineList.Insert(threeBeforePatch + 3, "\t\tif EID.isOnlineMultiplayer and Game():GetLevel():GetStage() >= LevelStage.Home then");
+                lineList.Insert(threeBeforePatch + 4, "\t\t\treturn listUpdatedForPlayers -- Calling player:HasCollectible can cause a crash after beating The Beast in R+ Coop");
+                lineList.Insert(threeBeforePatch + 5, "\t\tend");
+                File.WriteAllLines(eidAPIPath, lineList);
+            }
+            Console.WriteLine(eidApiModified.ToString());
+            Console.WriteLine(mainLuaModified.ToString());
+            return !(eidApiModified && mainLuaModified);
+        }
+    }
+}
--- IsaacOnlineModded/MainWindow.xaml.cs
+++ IsaacOnlineModded/MainWindow.xaml.cs
@@ -28,7 +28,35 @@ namespace IsaacModInstaller {
                 lblStatus.Foreground = System.Windows.Media.Brushes.Green;
             }
         }
+        private void EIDButton_Click(object sender, RoutedEventArgs e) {
+            string gamePath = txtGamePath.Text;
 
+            if (!File.Exists(gamePath)) {
+                MessageBox.Show("Invalid game path. Please select the correct executable.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var modsPath = Path.Combine(Path.GetDirectoryName(gamePath)!, "mods");
+            if (!Directory.Exists(modsPath)) {
+                MessageBox.Show($"No directory found at {modsPath}. Please make sure the mod is installed.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            var eidPath = Directory.GetDirectories(modsPath).FirstOrDefault(d => d.Contains("external", StringComparison.InvariantCultureIgnoreCase) && d.Contains("item", StringComparison.InvariantCultureIgnoreCase) && d.Contains("descriptions", StringComparison.InvariantCultureIgnoreCase));
+            if (eidPath == default) {
+                MessageBox.Show($"External Item Descriptions not found in mod dir {modsPath}. Please make sure the mod is installed.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            try {
+                bool success = EIDPatcher.Patch(eidPath!);
+
+                if (success) {
+                    lblStatus.Content = "EID patched successfully!";
+                    lblStatus.Foreground = System.Windows.Media.Brushes.Green;
+                } else {
+                    lblStatus.Content = "EID already patched.";
+                    lblStatus.Foreground = System.Windows.Media.Brushes.Orange;
+                }
+            } catch (Exception ex) {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
         private void PatchButton_Click(object sender, RoutedEventArgs e) {
             string gamePath = txtGamePath.Text;
 
